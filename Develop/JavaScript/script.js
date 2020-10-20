a17f7d92590f4bf72f31a573d137f26a3ac98a99
Truc Nguyen <trucn0215@gmail.com>
2020-10-19 18:33:41 -0700

    placing pseudocode in order

--- Develop/JavaScript/script.js
+++ Develop/JavaScript/script.js
@@ -1,6 +1,28 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//array list of numbers
+var nums = [0,1,2,3,4,5,6,7,8,9];
+
+//array list of lowercase letters
+
+//array list of UPPERCASE
+
+//array list of special characters
+
+//Password length
+
+
+function generatePassword() {
+  //confirm use number
+
+  //confirm use lowercase letters
+
+  //confirm use UPPERCASE letters
+
+  //confirm use special letter
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -11,17 +33,7 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); //when click the button 
 
-//if button click:
 //show series of promts for user to type in for password criterias
 //promt: which criteria to include in the password
-//question 1: lenght of password (8 to 128 length)
-//question 2: password include lowercase
-//question 3: password include updatercase
-//question 4: password include numeric
-//question 5: password include special characters
-
-//after all promt answered and validated, generate a random password that have all criterias.
-
-//Password either display in a alert or written to the page.
