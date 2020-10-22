1af6a7e1f27c6fe5479ba833d374f91c01366524
Truc Nguyen <trucn0215@gmail.com>
2020-10-21 18:34:07 -0700

    add more pseudocode as layout how things goes

--- Develop/JavaScript/script.js
+++ Develop/JavaScript/script.js
@@ -1,5 +1,5 @@
 // Assignment Code
-//let generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");
 
 var criteria = {
   //array list of numbers
@@ -12,10 +12,67 @@ var criteria = {
   "specicalChar": "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 }
 
-var generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+function generatePassword(){
+
+  // Prompt the user for a `passwordLength`
+  var passwordLength = prompt("Provide a length");
+
+  // If NOT (passwordLength >=8 || passwordLength <= 128)
+  // THEN ALERT to the user that they need to provide a correct length
+  // AND EXIT FUNCTION
+  if(numCon <=8 || numCon >= 128) {
+    alert("Please enter number between 8 to 128!")
+    return;
+  }
+
+  // DECLARE a new list of `characters`
+  var charactersToUse = [];
+
+  // CONFIRM if the password generator `isUsingNumbers`
+
+  // IF `isUsingNumber`
+  // THEN push `numbers` into `characterToUse` list
+  // AND APPEND on random number from the `number` list
+
+  // CONFIRM if the password generator `isUsingLowercaseLetter`
+
+  // IF `isUsingLowercaseLetter`
+  // THEN push `lowercaseLetter` into `isUsingLowercaseLetter`
+
+  // CONFIRM if the password generator `isUsingUppercaseLetter`
+
+  // IF `isUsingUppercaseLetter`
+  // THEN push `uppercaseLetter` into `isUsingUppercaseLetter`
+
+  // CONFIRM if the password generator `isUsingSpecialCharactersLetters`
+
+  // IF `isUsingSpecialCharactersLetters`
+  // THEN push `specialCharacters` into `characterToUse` list
+  
+  // IF NOT `characterToUse.length`
+  // THEN ALERT to the user that they need to provide a correct length
+  // AND EXIT FUNCTION
+
+  // FOR i = 0; CONTINUE as long as i < passwordLength; i++;
+  for( var i = 0; i < passwordLength; i++) {
+
+  }
+
+  // DECLARE a new `password` string
+  var password = "";
+
+  // WHILE password.Length < passwordLength
+  while( password.length < passwordLength) {
+    // SELECT `randomCharacter` a character from `charactersToUse`
+    // APPEND `randomCharacter` to `password` string
+  }
+
+  //RETURN `password`
+  return password;
+}
 // Write password to the #password input
 function writePassword() {
 
@@ -26,9 +83,7 @@ function writePassword() {
   var numb = confirm("Click OK if Password include numbers!");
   var specicalChar = confirm("Click OK if Password include special characters!");
 
-  if(numCon <=8 || numCon >= 128) {
-    alert("Please enter number between 8 to 128!")
-  }
+
 
 
   var password = generatePassword();
