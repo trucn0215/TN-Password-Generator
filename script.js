a8ee6a13753bb9010df1f455c1d49ed5c7ec6e59
Truc Nguyen <trucn0215@gmail.com>
2020-10-23 01:22:46 -0700

    adding all options and trying to generate random value

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
@@ -19,7 +17,6 @@ function writePassword() {
   var specicalCharacter = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
   
   // Prompt the user for a `passwordLength`
-  var passwordLength = parseInt(prompt("How many characters for your password? Enter a number between 8 to 128"));
 
   // Comfirming variables
   var lowercaseConfirm;
@@ -30,9 +27,12 @@ function writePassword() {
   var passwordPicking;
 
 function generatePassword(){
+  console.log (generatePassword);
+  var passwordLength = parseInt(prompt("How many characters for your password? Enter a number between 8 to 128"));
 
+  while (true) {
     // Condition for length input. If input is not valid, give ALERTs!
-    if (!passwordLength) {
+    if (passwordLength = NaN) {
       alert ("Please enter a number of Character for your Password!")
     }
     else if(passwordLength < 8 || passwordLength > 128) {
@@ -45,71 +45,80 @@ function generatePassword(){
       numberConfirm = confirm("Click OK if Password include numbers!");
       specicalCharConfirm = confirm("Click OK if Password include special characters!");
     }
-  }
-  // NO option picked
-  if (!lowercaseConfirm && !uppercaseConfirm && !numberConfirm && !specicalCharConfirm) {
-    passwordPicking = alert("Password need at least ONE criteria!");
-  }
-  // 4 option picked
-  else if (lowercaseConfirm && uppercaseConfirm && numberConfirm && specicalCharConfirm){
-    passwordPicking = lowercase.concat(numberic, uppercase, specicalCharacter);
-  }
+  //   break;
+  // }
 
-  // 3 option picked
-  else if (lowercaseConfirm && uppercaseConfirm && numberConfirm) {
-    passwordPicking = lowercase.concat(numberic, uppercase);
-  }
-  else if (lowercaseConfirm && uppercaseConfirm && specicalCharConfirm) {
-    passwordPicking = lowercase.concat(uppercase, specicalCharacter);
-  }
-  else if (lowercaseConfirm && specicalCharConfirm && numberConfirm) {
-    passwordPicking = lowercase.concat(specicalCharacter, numberic);
-  }
-  else if (uppercaseConfirm && specicalCharConfirm && numberConfirm) {
-    passwordPicking = numberic.concat(uppercase, specicalCharacter);
-  }
+  // while (true) {
+    // NO option picked
+    if (!lowercaseConfirm && !uppercaseConfirm && !numberConfirm && !specicalCharConfirm) {
+      passwordPicking = alert("Password need at least ONE criteria!");
+    }
+    // 4 option picked
+    else if (lowercaseConfirm && uppercaseConfirm && numberConfirm && specicalCharConfirm){
+      passwordPicking = lowercase.concat(numberic, uppercase, specicalCharacter);
+    }
 
-  // 2 options picked
-  else if (lowercaseConfirm && uppercaseConfirm) {
-    passwordPicking = lowercase.concat(uppercase);
-  }
-  else if (lowercaseConfirm && numberConfirm) {
-    passwordPicking = lowercase.concat(numberic);
-  }
-  else if (lowercaseConfirm && specicalCharConfirm) {
-    passwordPicking = lowercase.concat(specicalCharacter);
-  }
-  else if (uppercaseConfirm && numberConfirm) {
-    passwordPicking = uppercase.concat(numberic);
-  }
-  else if (uppercaseConfirm && specicalCharConfirm) {
-    passwordPicking = uppercase.concat(specicalCharacter);
-  }
-  else if (numberConfirm && specicalCharConfirm) {
-    passwordPicking = numberic.concat(specicalCharacter);
-  }
+    // 3 option picked
+    else if (lowercaseConfirm && uppercaseConfirm && numberConfirm) {
+      passwordPicking = lowercase.concat(numberic, uppercase);
+    }
+    else if (lowercaseConfirm && uppercaseConfirm && specicalCharConfirm) {
+      passwordPicking = lowercase.concat(uppercase, specicalCharacter);
+    }
+    else if (lowercaseConfirm && specicalCharConfirm && numberConfirm) {
+      passwordPicking = lowercase.concat(specicalCharacter, numberic);
+    }
+    else if (uppercaseConfirm && specicalCharConfirm && numberConfirm) {
+      passwordPicking = numberic.concat(uppercase, specicalCharacter);
+    }
 
-  // 1 option picked
-  else if (lowercaseConfirm) {
-    passwordPicking = lowercase;
-  }
-  else if (uppercaseConfirm) {
-    passwordPicking = uppercase;
-  }
-  else if (numberConfirm) {
-    passwordPicking = numberic;
-  }
-  else if (specicalCharConfirm) {
-    passwordPicking = specicalCharacter;
-  }
+    // 2 options picked
+    else if (lowercaseConfirm && uppercaseConfirm) {
+      passwordPicking = lowercase.concat(uppercase);
+    }
+    else if (lowercaseConfirm && numberConfirm) {
+      passwordPicking = lowercase.concat(numberic);
+    }
+    else if (lowercaseConfirm && specicalCharConfirm) {
+      passwordPicking = lowercase.concat(specicalCharacter);
+    }
+    else if (uppercaseConfirm && numberConfirm) {
+      passwordPicking = uppercase.concat(numberic);
+    }
+    else if (uppercaseConfirm && specicalCharConfirm) {
+      passwordPicking = uppercase.concat(specicalCharacter);
+    }
+    else if (numberConfirm && specicalCharConfirm) {
+      passwordPicking = numberic.concat(specicalCharacter);
+    }
 
+    // 1 option picked
+    else if (lowercaseConfirm) {
+      passwordPicking = lowercase;
+    }
+    else if (uppercaseConfirm) {
+      passwordPicking = uppercase;
+    }
+    else if (numberConfirm) {
+      passwordPicking = numberic;
+    }
+    else if (specicalCharConfirm) {
+      passwordPicking = specicalCharacter;
+    }
+    break;
+  }
   // DECLARE a new list of `characters`
   var password = [];
+  var randomPassword = "";
 
-  // FOR i = 0; CONTINUE as long as i < passwordLength; i++;
+  // generate random selection
   for( var i = 0; i < passwordPicking; i++) {
-    password = password + charactersToUse[Math.floor(Math.random() * charactersToUse.length)];
+    randomPassword = passwordPicking[Math.floor(Math.random() * passwordPicking.length)];
+    password.push(randomPassword);
   }
-
+}
   //RETURN `password`
-  return password;
+ // return password;
+
+ // Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
