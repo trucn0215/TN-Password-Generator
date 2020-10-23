a4c2017704df5e294ef757baa83c296934e69c2d
Truc Nguyen <trucn0215@gmail.com>
2020-10-23 00:58:03 -0700

    add when only 2 option picked and 1 option picked

--- script.js
+++ script.js
@@ -22,24 +22,84 @@ function generatePassword(){
   // Prompt the user for a `passwordLength`
   var passwordLength = parseInt(prompt("How many characters for your password? Enter a number between 8 to 128"));
 
-  // Check to making sure the number stay in range from 8 to 128. If not, ALERT user to put a number that in the range
-  while(passwordLength <=7 || passwordLength >= 128) {
+  // Comfirming variables
+  var lowercaseConfirm;
+  var uppercaseConfirm;
+  var numberConfirm;
+  var specicalCharConfirm;
+
+  var passwordPicking;
+
+    // Condition for length input. If input is not valid, give ALERTs!
+    if (!passwordLength) {
+      alert ("Please enter a number of Character for your Password!")
+    }
+    else if(passwordLength < 8 || passwordLength > 128) {
     alert("Please enter number between 8 to 128!")
-    var passwordLength = parseInt(prompt("How many characters for your password? Enter a number between 8 to 128"));
+    passwordLength = parseInt(prompt("How many characters for your password? Enter a number between 8 to 128"));
+    }
+    else {
+      lowercaseConfirm = confirm("Click OK if Password include lower case!");
+      uppercaseConfirm = confirm("Click OK if Password include UPPER case!");
+      numberConfirm = confirm("Click OK if Password include numbers!");
+      specicalCharConfirm = confirm("Click OK if Password include special characters!");
+    }
+  }
+  // NO option picked
+  if (!lowercaseConfirm && !uppercaseConfirm && !numberConfirm && !specicalCharConfirm) {
+    passwordPicking = alert("Password need at least ONE criteria!");
+  }
+  // 4 option picked
+  else if (lowercaseConfirm && uppercaseConfirm && numberConfirm && specicalCharConfirm){
+    passwordPicking = lowercase.concat(numberic, uppercase, specicalCharacter);
   }
 
-    // series of prompt and confirming question for the password
-    var lowercaseConfirm = confirm("Click OK if Password include lower case!");
-    var uppercaseConfirm = confirm("Click OK if Password include UPPER case!");
-    var numberConfirm = confirm("Click OK if Password include numbers!");
-    var specicalCharConfirm = confirm("Click OK if Password include special characters!");
+  // 3 option picked
+  else if (lowercaseConfirm && uppercaseConfirm && numberConfirm) {
+    passwordPicking = lowercase.concat(numberic, uppercase);
+  }
+  else if (lowercaseConfirm && uppercaseConfirm && specicalCharConfirm) {
+    passwordPicking = lowercase.concat(uppercase, specicalCharacter);
+  }
+  else if (lowercaseConfirm && specicalCharConfirm && numberConfirm) {
+    passwordPicking = lowercase.concat(specicalCharacter, numberic);
+  }
+  else if (uppercaseConfirm && specicalCharConfirm && numberConfirm) {
+    passwordPicking = numberic.concat(uppercase, specicalCharacter);
+  }
 
-  while(lowercaseConfirm ==false && uppercaseConfirm == false && numberConfirm == false && specicalCharConfirm == false) {
-    alert("Password need at least ONE criteria!");
-    var lowercaseConfirm = confirm("Click OK if Password include lower case!");
-    var uppercaseConfirm = confirm("Click OK if Password include UPPER case!");
-    var numberConfirm = confirm("Click OK if Password include numbers!");
-    var specicalCharConfirm = confirm("Click OK if Password include special characters!");
+  // 2 options picked
+  else if (lowercaseConfirm && uppercaseConfirm) {
+    passwordPicking = lowercase.concat(uppercase);
+  }
+  else if (lowercaseConfirm && numberConfirm) {
+    passwordPicking = lowercase.concat(numberic);
+  }
+  else if (lowercaseConfirm && specicalCharConfirm) {
+    passwordPicking = lowercase.concat(specicalCharacter);
+  }
+  else if (uppercaseConfirm && numberConfirm) {
+    passwordPicking = uppercase.concat(numberic);
+  }
+  else if (uppercaseConfirm && specicalCharConfirm) {
+    passwordPicking = uppercase.concat(specicalCharacter);
+  }
+  else if (numberConfirm && specicalCharConfirm) {
+    passwordPicking = numberic.concat(specicalCharacter);
+  }
+
+  // 1 option picked
+  else if (lowercaseConfirm) {
+    passwordPicking = lowercase;
+  }
+  else if (uppercaseConfirm) {
+    passwordPicking = uppercase;
+  }
+  else if (numberConfirm) {
+    passwordPicking = numberic;
+  }
+  else if (specicalCharConfirm) {
+    passwordPicking = specicalCharacter;
   }
 
   // DECLARE a new list of `characters`
@@ -87,4 +147,3 @@ function generatePassword(){
 
   //RETURN `password`
   return password;
-}
